c47d8c11eb564f28995fd1dde0b608f9a4768f82
Vlad Stein <in7byss@gmail.com>
2020-10-25 13:50:15 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let start = 1;
+
+  while (start === 1) {
+
+    let newStr = str;
+
+    bracketsConfig.map(element => str = str.replace(element[0] + element[1], ''));
+
+    if (newStr === str) {
+      start = 0;
+    }
+
+  }
+  return (str.length === 0) ? true : false
 }
